e6dff6cfd3ddd6e5b8ff6e5ffc8012a46638c78b
PatatStinus <stijnrpd@gmail.com>
2022-02-16 23:12:16 +0100

    Project

---
+++ Source/RoboCopp/RoboCoppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RoboCoppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOCOPP_API ARoboCoppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
